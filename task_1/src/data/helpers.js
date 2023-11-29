5880d1f51c066f339d472df8e6dd55910bcaf36f
mike-devasy <108010984+mike-devasy@users.noreply.github.com>
2023-11-29 13:00:42 +0200

    Delete task_1 directory

--- task_1/src/data/helpers.js
+++
@@ -1,9 +0,0 @@
-export function getStudentsArrayTo5(studentArray){
-	return studentArray.map((student) => ({
-		...student,
-		averageScore: getAverageScore5(student.averageScore)
-	}))
-}
-	export function getAverageScore5(averageScore){
-    return  Math.round(averageScore / 2.4) 
-	}
